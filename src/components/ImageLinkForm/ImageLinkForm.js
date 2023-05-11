e3f892a415a426436d63ce405e0d628ea96bcae1
va-run-6626 <vbhutada143@gmail.com>
2023-05-11 15:37:51 +0530

    Varun's contribution

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () =>{
+const ImageLinkForm = ({onInputChange}) =>{
     return(
         <div>
             <p className="f3">
@@ -9,7 +9,7 @@ const ImageLinkForm = () =>{
             </p>
            <div className="center">
                 <div className="form .pattern-grid-lg center pa4 br3 shadow-1">
-                    <input className="f4 pa2 fn w-75" type="text"></input>
+                    <input className="f4 pa2 fn w-75" type="text" onChange={onInputChange}></input>
                     <button className="fn w-25 grow f4 link ph3 pv2 dib white bg-purple ">Detect</button>
                 </div>
             </div>
